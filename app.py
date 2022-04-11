43076fec81d8e34459931b5b6cf07700815af3b9
Franco Naghetini <franconaghetini@gmail.com>
2022-04-11 10:06:56 -0300

    First commit

---
+++ app.py
@@ -0,0 +1,88 @@
+from tkinter import *
+from glob import glob
+import pandas as pd
+import data_handler
+from tkinter import messagebox
+from idlelib.tooltip import Hovertip
+
+######################################################################################
+# ------------------------------------- Função ------------------------------------- #
+######################################################################################
+
+
+def btn_execute():
+    folder_path = tbx_table.get("1.0", "end-1c").replace('\\', '/')
+    input_files_list = [f.replace('\\', '/') for f in glob(f"{folder_path}/*.xlsx")]
+    sheet = tbx_sheet.get("1.0", "end-1c")
+    cols = ["A,B,C,F:" + col for col in tbx_lastcol.get("1.0", "end-1c").split(sep=',')]
+    output_files_list = [f[:-5] + '_SCD.csv' for f in input_files_list]
+
+    index = ["Holeid", "parent_sample_number",
+             "Sample number", "Dispatch",
+             "date_shipped", "Assay sample type",
+             "Lab", "lab_reference_number",
+             "analysis_date"]
+
+    rename = {"Holeid": "hole_number",
+              "Sample number": "sample_number",
+              "Assay sample type": "sample_type",
+              "Dispatch": "dispatch_number"}
+
+    if folder_path == '' or sheet == '' or cols == '':
+        messagebox.showerror('Atenção', "Por favor, preencha todos os campos")
+    elif len(input_files_list) != len(cols):
+        messagebox.showerror('Atenção', f"O número de colunas não é igual ao número de arquivos na pasta {folder_path}")
+    else:
+        for in_f, c, out_f in zip(input_files_list, cols, output_files_list):
+            df = pd.read_excel(in_f, sheet_name=sheet, header=0, usecols=c)
+            stacked = data_handler.stack_data_frame(df, index, rename)
+            data_handler.column_name_manipulation(stacked)
+            stacked.to_csv(out_f, encoding='cp1252')
+        messagebox.showinfo('Script Concluído', f'Arquivos gerados com sucesso na pasta {folder_path}!')
+
+######################################################################################
+# ------------------------------------ Interface ----------------------------------- #
+######################################################################################
+
+
+# Criação da janela principal
+root = Tk()
+
+# Ícone
+root.wm_iconbitmap('datamine.ico')
+# Título
+root.title("Datamine GDMS")
+# Dimensões da tabela
+root.geometry("310x210")
+# Configuração de background
+root.configure(background='white')
+
+# Widgets
+txt_title = Label(root, text="Geração Sample Column Details", bg='white', fg='black', font="lucida 12 bold")
+txt_table = Label(root, text="Diretório das Tabelas:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+tbx_table = Text(root, height=1, width=20, bg='light yellow')
+txt_sheet = Label(root, text="Nome da Aba:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+tbx_sheet = Text(root, height=1, width=20, bg='light yellow')
+txt_lastcol = Label(root, text="Últimas Colunas:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
+tbx_lastcol = Text(root, height=1, width=20, bg='light yellow')
+btn_exec = Button(root, text="Executar", width=15, command=lambda: btn_execute())
+txt_version = Label(root, text="v0.0.1", bg='white', fg='black', justify=RIGHT, anchor='e')
+
+# Associando widgets à janela principal
+txt_title.grid(row=0, column=0, columnspan=2, pady=10)
+txt_table.grid(row=1, column=0, pady=5, sticky=W)
+tbx_table.grid(row=1, column=1)
+txt_sheet.grid(row=2, column=0, pady=5, sticky=W)
+tbx_sheet.grid(row=2, column=1)
+txt_lastcol.grid(row=3, column=0, pady=5, sticky=W)
+tbx_lastcol.grid(row=3, column=1)
+btn_exec.grid(row=4, column=0, columnspan=2, pady=10)
+txt_version.grid(row=5, column=1, sticky=E)
+
+# Tooltips
+tip_table = Hovertip(tbx_table, "Caminho para a pasta \nque contém as tabelas \nde entrada.")
+tip_sheet = Hovertip(tbx_sheet, "Nome da aba dos arquivos Excel. \nPor padrão, adota-se 'sample'.")
+tip_lastcols = Hovertip(tbx_lastcol, "Nomes das últimas colunas \nseparados por vírgula. \nExemplo: AT,R,AV,AV,AS.")
+
+# Execução do app
+root.mainloop()
---
+++ data_handler.py
@@ -0,0 +1,128 @@
+#encoding: utf-8
+import pandas as pd
+# import progressbar
+from datetime import date
+
+# bar = progressbar.ProgressBar(maxval=df.shape[0], widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
+# bar.start()
+# bar.update(index)
+# bar.finish()
+
+
+def stack_data_frame(df, index, rename):
+    stacked = df.set_index(index).stack()
+    df = stacked.reset_index()
+    level_column = "_".join(["level", str(len(index))])
+    rename.update({level_column: 'column_name', 0: 'original_result'})
+    df.rename(columns=rename, inplace=True)
+    return df
+
+
+def column_name_manipulation(df):
+    # standard_code   lab_reference_number    analysis_date   dispatch_number date_shipped
+    # df['standard_code'] = df['Standard id']
+    df['math_performed'] = df.apply(lambda row: __check_min_max(row), axis=1)
+    df['action_reason'] = df.apply(lambda row: __check_min_max_reason(row), axis=1)
+    df['original_element'] = df.apply(lambda row: __get_original_element(row), axis=1)
+    df['date_imported'] = date.today()
+    df['imported_by'] = 'Datamine'
+    df['original_result_number'] = df.apply(lambda row: __get_original_value(row), axis=1)
+    df['result_after_math'] = df.apply(lambda row: __convert_to_original(row), axis=1)
+    df['unit_of_measure'] = df.apply(lambda row: __get_unity_measure(row), axis=1)
+    df['analytical_technique'] = 'ICPASS'
+    df['status'] = None
+    df['lab_element'] = df['original_element']
+    df['module_name'] = 'DHL'
+    df['laboratory_id'] = df.apply(lambda row: __make_lab_correspondence(row, 'other'), axis=1)
+    df['laboratory_name'] = None
+    df['date_received'] = None
+    df['lab_analytical_method'] = df.apply(lambda row: __get_lab_analytical_method(row), axis=1)
+    df['sample_status'] = None
+    df['lab_method_code'] = df['lab_analytical_method']
+    df['lab_assay_uofm'] = df.apply(lambda row: __get_unity_measure_lab(row), axis=1)
+    df['standard_min_value'] = None
+    df['standard_max_value'] = None
+    df['standard_deviation'] = None
+    df['column_name'] = df.apply(lambda row: __correct_colum_label(row), axis=1)
+
+
+def __correct_colum_label(row):
+    column_label = row['column_name']
+    # column_label  = column_label[:7] + "Lab"
+    return column_label
+
+
+def __get_original_element(row):
+    column_label = row['column_name']
+    element = column_label.split("_")[0]
+    return element
+
+
+def __get_unity_measure(row):
+    column_label = row['column_name']
+    element = column_label.split("_")[1]
+    return element
+
+
+def __get_unity_measure_lab(row):
+    column_label = row['column_name']
+    element = column_label.split("_")[1]
+    if element == 'ozt':
+        return 'ozt'
+    return '%'
+
+
+def __make_lab_correspondence(row, field='index'):
+    lookup_table = {
+        'ALS':(2, 'ALS'),
+        'INSP':(12, 'Inspectorate'),
+        'NDEF':(23, u'Não Definido'),
+        'SGSPE':(26, u'SGS Perú')
+    }
+    lab = row['Lab']
+    if field == 'index':
+        return lookup_table.get(lab, (0, lab))[0]
+    return lookup_table.get(lab, (0, lab))[1]
+
+
+def __get_lab_analytical_method(row):
+    column_label = row['column_name']
+    analytical_method = column_label.split('_')[2]
+    return analytical_method
+
+
+def math_permormance(df):
+    df['math_performed'] = df.apply(lambda row: __check_min_max(row), axis=1)
+    df['math_reason'] = df.apply(lambda row: __check_min_max_reason(row), axis=1)
+    df['original_result_number'] = df.apply(lambda row: __convert_to_original(row), axis=1)
+    df['result_after_math'] = df['original_result_number']
+
+
+def __check_min_max(row):
+    if '<' in str(row['original_result']):
+        return '/2'
+    elif '>' in str(row['original_result']):
+        return '*1'
+
+
+def __check_min_max_reason(row):
+    if '<' in str(row['original_result']):
+        return 'abaixo LD'
+    elif '>' in str(row['original_result']):
+        return 'acima LD'
+
+
+def __get_original_value(row):
+    value = row['original_result']
+    if '<' in str(value) or '>' in str(value):
+        value = str(value)[1:]
+    return value
+
+
+def __convert_to_original(row):    
+    value = row['original_result']
+    if '<' in str(value):
+        value = float(value[1:])/2
+    elif '>' in str(value):
+        value = float(value[1:])
+    return value
