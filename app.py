360af54d04837d01a24affa50431fbfb8485049e
Franco Naghetini <franconaghetini@gmail.com>
2022-04-18 11:00:01 -0300

    Remove sheet textbox from UI

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ from idlelib.tooltip import Hovertip
 def btn_execute():
     folder_path = tbx_table.get("1.0", "end-1c")
     input_files_list = [f.replace('\\', '/') for f in glob(f"{folder_path}/*.xlsx")]
-    sheet = tbx_sheet.get("1.0", "end-1c")
     cols = ["A:" + col for col in tbx_lastcol.get("1.0", "end-1c").split(sep=',')]
     output_files_list = [f[:-5] + '_SCD.csv' for f in input_files_list]
 
@@ -35,7 +34,7 @@ def btn_execute():
                  'analytical_technique', 'sample_type', 'lab_element', 'module_name', 'laboratory_id',
                  'laboratory_name', 'parent_sample_number', 'lab_method_code', 'lab_assay_uofm']
 
-    if folder_path == '' or sheet == '' or cols == '':
+    if folder_path == '' or cols == '':
         messagebox.showerror('Erro', "Por favor, preencha todos os campos!")
     elif len(input_files_list) == 0:
         messagebox.showerror('Erro', f"Não há arquivos .xlsx na pasta {folder_path}.")
@@ -43,7 +42,7 @@ def btn_execute():
         messagebox.showerror('Erro', f"O número de últimas colunas ({len(cols)}) não é igual ao número de arquivos ({len(input_files_list)}) na pasta {folder_path}.")
     else:
         for in_f, c, out_f in zip(input_files_list, cols, output_files_list):
-            df = pd.read_excel(in_f, sheet_name=sheet, header=0, usecols=c)
+            df = pd.read_excel(in_f, sheet_name='sample', header=0, usecols=c)
             df_methods = pd.read_excel(in_f, sheet_name='methods', header=0, usecols='A:B', index_col=0)
             dict_methods = df_methods.to_dict(orient='dict')['method']
             df_labs = pd.read_excel(in_f, sheet_name='labs', header=0, usecols='A:B', index_col=0)
@@ -67,7 +66,7 @@ root.wm_iconbitmap('datamine.ico')
 # Título
 root.title("Datamine GDMS")
 # Dimensões da tabela
-root.geometry("310x210")
+root.geometry("310x180")
 # Configuração de background
 root.configure(background='white')
 
@@ -75,8 +74,6 @@ root.configure(background='white')
 txt_title = Label(root, text="Geração Sample Column Details", bg='white', fg='black', font="lucida 12 bold")
 txt_table = Label(root, text="Diretório das Tabelas:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_table = Text(root, height=1, width=20, bg='light yellow')
-txt_sheet = Label(root, text="Nome da Aba:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
-tbx_sheet = Text(root, height=1, width=20, bg='light yellow')
 txt_lastcol = Label(root, text="Últimas Colunas:", bg='white', fg='black', justify=LEFT, anchor='w', padx=10)
 tbx_lastcol = Text(root, height=1, width=20, bg='light yellow')
 btn_exec = Button(root, text="Executar", width=15, command=lambda: btn_execute())
@@ -86,8 +83,6 @@ txt_version = Label(root, text="v0.0.2", bg='white', fg='black', justify=RIGHT, 
 txt_title.grid(row=0, column=0, columnspan=2, pady=10)
 txt_table.grid(row=1, column=0, pady=5, sticky=W)
 tbx_table.grid(row=1, column=1)
-txt_sheet.grid(row=2, column=0, pady=5, sticky=W)
-tbx_sheet.grid(row=2, column=1)
 txt_lastcol.grid(row=3, column=0, pady=5, sticky=W)
 tbx_lastcol.grid(row=3, column=1)
 btn_exec.grid(row=4, column=0, columnspan=2, pady=10)
@@ -95,7 +90,6 @@ txt_version.grid(row=5, column=1, sticky=E)
 
 # Tooltips
 tip_table = Hovertip(tbx_table, "Caminho para a pasta \nque contém as tabelas \nde entrada.")
-tip_sheet = Hovertip(tbx_sheet, "Nome da aba dos arquivos Excel. \nPor padrão, adota-se 'sample'.")
 tip_lastcols = Hovertip(tbx_lastcol, "Nomes das últimas colunas \nseparados por vírgula. \nExemplo: AT,R,AV,AV,AS.")
 
 # Execução do app
